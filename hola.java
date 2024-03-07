e0eb88aff8ff5f1fe975f9909e11e0d5c986356a
hugomttn <hugomartinn18@gmail.com>
2024-03-07 15:58:04 +0100

    Cambios de alumno B

--- hola.java
+++ hola.java
@@ -2,5 +2,6 @@ public class hola {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Hola gente");
+		System.out.println("Qué pasa Jesús");
 	}
 }
